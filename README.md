c8b64377eaf1d9e856304611d59ff2c2f1dbed70
Bbesniku7 <Bbesniku7>
2022-05-14 17:54:41 +0200

    created generator for random users

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # gen-random-user-with-vue
 This is the first project , and simple project to get started with the vue
+
+
+# about project 
+Project is created with the CDN (content delivery network), for that resone we have the access to the Vue.createApp and other things, is the frist project and its simple. 
---
+++ index.js
@@ -0,0 +1,31 @@
+
+const app = Vue.createApp({
+    data() {
+        return {
+            firstName: 'Besnik',
+            lastName: 'Quanku',
+            email: 'besnik@gmail.com',
+            gender: 'male',
+            picture: 'https://randomuser.me/api/portraits/men/10.jpg'
+        }
+    },
+    methods: {
+      async getUser() { 
+        try{
+            const res = await fetch('https://randomuser.me/api')
+            const {results} = await res.json()
+            this.firstName = results[0].name.first
+            this.lastName = results[0].name.last
+            this.email = results[0].email 
+            this.gender = results[0].gender
+            this.picture = results[0].picture.large
+
+        }catch(error){
+            console.log(error)
+        }
+      }
+    }
+})
+
+
+app.mount('#app')  
